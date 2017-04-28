653626463ada431933409a28dcaa478d8048621a
Harselan <haeressel@gmail.com>
2017-04-28 22:45:37 +0200

    First commit
    Attempt to picture the project in HTML

---
+++ index.php
@@ -0,0 +1,95 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href="styles/css.css" rel="stylesheet">
+        <title>harald-issues-projekt</title>
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="inlagg">
+                <div class="header">
+                    <h1>Titel</h1>
+                    <h2>Problemet</h2>
+                </div>
+                <div class="content">
+                    <pre>
+$(document).ready(function (){
+    $("#nyhetbutton").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#nyheter").offset().top
+        }, 500);
+       });
+   });
+$(document).ready(function (){
+     $("#kontaktbutton").click(function (){
+        $('html, body').animate({
+              scrollTop: $("#kontakt").offset().top
+           }, 1000);
+       });
+});
+$(document).ready(function (){
+    $("#nyhetbutton").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#nyheter").offset().top
+        }, 500);
+       });
+   });
+$(document).ready(function (){
+     $("#kontaktbutton").click(function (){
+        $('html, body').animate({
+              scrollTop: $("#kontakt").offset().top
+           }, 1000);
+       });
+});
+$(document).ready(function (){
+    $("#nyhetbutton").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#nyheter").offset().top
+        }, 500);
+       });
+   });
+$(document).ready(function (){
+     $("#kontaktbutton").click(function (){
+        $('html, body').animate({
+              scrollTop: $("#kontakt").offset().top
+           }, 1000);
+       });
+});
+$(document).ready(function (){
+    $("#nyhetbutton").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#nyheter").offset().top
+        }, 500);
+       });
+   });
+$(document).ready(function (){
+     $("#kontaktbutton").click(function (){
+        $('html, body').animate({
+              scrollTop: $("#kontakt").offset().top
+           }, 1000);
+       });
+});
+$(document).ready(function (){
+    $("#nyhetbutton").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#nyheter").offset().top
+        }, 500);
+       });
+   });
+$(document).ready(function (){
+     $("#kontaktbutton").click(function (){
+        $('html, body').animate({
+              scrollTop: $("#kontakt").offset().top
+           }, 1000);
+       });
+});
+                    </pre>
+                </div>
+                <div class="commentfield">
+                    <div class="comment"></div>
+                    <button>Kommentera</button>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
