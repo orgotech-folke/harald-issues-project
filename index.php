28e25c56ac177e182a858e3fc7740621ce8b21fd
Harselan <haeressel@gmail.com>
2017-05-02 19:51:51 +0200

    Har byggt upp databas och visar upp projekten
    Jag har byggt upp en mysql databas som håller alla projekts data och
    dess problem. Sedan skrivs problemen ut nyast först!

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+﻿<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "projectsissues";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+?>
 <html>
     <head>
         <meta charset="utf-8">
@@ -6,51 +14,35 @@
     </head>
     <body>
         <div class="wrapper">
-            <div class="issue">
-                    <table cellspacing = "5">
-                        <tr>
-                            <td class="state"><div class="open"></div></td>
-                            <td>projectname</td>
-                            <td>problemname</td>
-                            <td>Short description</td>
-                            <td><a href="problemsite.php">Läs mer</a></td>
-                        </tr>
-                    </table>
-                </div>
-            <div class="inlagg">
-                <div class="header">
-                    <h1>Titel</h1>
-                </div>
-                <div class="content">
-                    <pre>
-$(document).ready(function (){
-     $("#kontaktbutton").click(function (){
-        $('html, body').animate({
-              scrollTop: $("#kontakt").offset().top
-           }, 1000);
-       });
-});
-$(document).ready(function (){
-    $("#nyhetbutton").click(function (){
-        $('html, body').animate({
-            scrollTop: $("#nyheter").offset().top
-        }, 500);
-       });
-   });
-$(document).ready(function (){
-     $("#kontaktbutton").click(function (){
-        $('html, body').animate({
-              scrollTop: $("#kontakt").offset().top
-           }, 1000);
-       });
-});
-                    </pre>
-                </div>
-                <div class="commentfield">
-                    <textarea style="float:left; width:85%;"></textarea>
-                    <button>Kommentera</button>
-                </div>
-            </div>
+            <?php
+            $max = mysqli_query($conn, "SELECT MAX(id) as MAX FROM projects");
+            $row = mysqli_fetch_object($max);
+            $max = $row->MAX;
+            for($i = $max; $i > 0; $i--)
+            {        
+                $result = mysqli_query($conn, "SELECT * FROM issues WHERE id = $i");
+                while($row = mysqli_fetch_object($result))
+                {
+                    $projectname = $row->PROJECTNAME;
+                    echo "<div class='issue'><table cellspacing = '5'>
+                    <tr>
+                    <td class='state'><div class='open'></div></td>
+                    <td>".$row->NAME."</td>
+                    <td>".$row->PROJECTNAME."</td>
+                    <td>".$row->PROBLEM."</td>
+                    <td><form action='problemsite.php' method='post'><input type='hidden' name='project' value='".$projectname."'><input type='submit'value='Läs mer'></td>
+                    </tr></table>";
+            ?>
+            <?php 
+                    $resulta = mysqli_query($conn, "SELECT CODE FROM projects WHERE PROJECTNAME = '".$projectname."'");
+                    $htmlcode = "No code found";
+                    while($rowa = mysqli_fetch_object($resulta)){
+                        $htmlcode = htmlspecialchars($rowa->CODE);
+                    };
+                    echo "<div class='inlagg'><div class='header'><h1>".$row->PROJECTNAME."</h1><div class='content'><pre>$htmlcode</pre></div></div></div>";
+                }
+            }
+            ?>
         </div>
     </body>
 </html>
--- problemsite.php
+++ problemsite.php
@@ -8,32 +8,10 @@
         <div class="wrapper">
             <div class="inlagg">
                 <div class="header">
-                    <h1>Titel</h1>
+                    <h1><?php echo $_POST['project'];?></h1>
                 </div>
                 <div class="content">
-                    <pre>
-$(document).ready(function (){
-     $("#kontaktbutton").click(function (){
-        $('html, body').animate({
-              scrollTop: $("#kontakt").offset().top
-           }, 1000);
-       });
-});
-$(document).ready(function (){
-    $("#nyhetbutton").click(function (){
-        $('html, body').animate({
-            scrollTop: $("#nyheter").offset().top
-        }, 500);
-       });
-   });
-$(document).ready(function (){
-     $("#kontaktbutton").click(function (){
-        $('html, body').animate({
-              scrollTop: $("#kontakt").offset().top
-           }, 1000);
-       });
-});
-                    </pre>
+                  
                 </div>
                 <div class="commentfield">
                     <textarea style="float:left; width:85%;"></textarea>
